77eef0b9c6d5887cdf388e73557dcbae294ba17a
Gordon Arber <Gordon.arber@gmail.com>
2021-01-29 11:37:21 -0500

    init cleanup
    cleaned up boiler plate

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDO2TV4P1zPPE283JwV4tMOtN3CpFsabCo",
+  authDomain: "chat-app-a8200.firebaseapp.com",
+  projectId: "chat-app-a8200",
+  storageBucket: "chat-app-a8200.appspot.com",
+  messagingSenderId: "938170654364",
+  appId: "1:938170654364:web:014d47bde2ff94138a5759",
+};
